d54a4b885189870277397cc1f53f655474433448
Irina_Mironova <{missirena@mail.ru}>
2024-06-16 10:09:19 +0300

    Homework3

--- src/main/java/ru/otus/java/homework3/MainApplication3.java
+++ src/main/java/ru/otus/java/homework3/MainApplication3.java
@@ -1,10 +1,10 @@
 package ru.otus.java.homework3;
 
-public class MainApplicaton3 {
+public class MainApplication3 {
 
     public static void main(String[] args) {
-        int[][] array = {{1, 2, 123},{7, -8, -11},{1, 0, 120003}};
-        int[][] array2 = {{7, 8, 326},{72, 811, 101},{-1, 0, -120003}};
+        int[][] array = {{1, 2, 123}, {7, -8, -11}, {1, 0, 120003}};
+        int[][] array2 = {{7, 8, 326}, {72, 811, 101}, {-1, 0, -120003}};
         sumOfPositiveElements(array);
         print2DArray(6);
         findMax(array);
@@ -58,13 +58,14 @@ public class MainApplicaton3 {
         }
         return max;
     }
+
     public static int sumOfElementsSecondRow(int[][] array) {
         int sumSecondRow = 0;
         for (int i = 1; i < 2; i++) {
             for (int j = 0; j < array[i].length; j++) {
-                if (i>=1)
-                sumSecondRow += array[1][j];
-                else sumSecondRow=-1;
+                if (i >= 1)
+                    sumSecondRow += array[1][j];
+                else sumSecondRow = -1;
             }
         }
         System.out.println(sumSecondRow);
