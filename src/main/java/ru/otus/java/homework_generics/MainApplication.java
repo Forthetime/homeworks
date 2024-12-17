f8378a1a2e877a83cb5e28f48589493816047dab
Irina_Mironova <{missirena@mail.ru}>
2024-12-17 07:42:28 +0400

    Homework_generics

---
+++ src/main/java/ru/otus/java/homework_generics/MainApplication.java
@@ -0,0 +1,26 @@
+package ru.otus.java.homework_generics;
+
+public class MainApplication {
+    public static void main(String[] args) {
+        Apple apple = new Apple(20);
+        Orange orange1 = new Orange(32);
+        Box<Orange> orangeBox1 = new Box<>();
+        Box<Apple> appleBox1 = new Box<>();
+        Box<Fruit> mixBox1 = new Box<>();
+        orangeBox1.addFruit(orange1);
+        orangeBox1.addFruit(new Orange(16));
+        mixBox1.addFruit(new Orange(20));
+        mixBox1.addFruit(new Apple(28));
+        System.out.println(orangeBox1);
+        appleBox1.addFruit(apple);
+        appleBox1.addFruit(new Apple(34));
+        System.out.println(appleBox1);
+        orangeBox1.calculateWeight();
+        appleBox1.calculateWeight();
+        mixBox1.calculateWeight();
+        orangeBox1.compareWeightOfBoxes(mixBox1);
+        mixBox1.poar(appleBox1);
+        mixBox1.calculateWeight();
+        mixBox1.calculateWeight();
+    }
+}
---
+++ src/main/java/ru/otus/java/homework_generics/OrangeBox.java
@@ -0,0 +1,6 @@
+package ru.otus.java.homework_generics;
+
+public class OrangeBox extends Box<Orange>{
+    public OrangeBox() {
+    }
+}
