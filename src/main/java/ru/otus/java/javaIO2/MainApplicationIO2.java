6b217cfd3504bee0817714b0e3b724ac4300a860
Irina_Mironova <{missirena@mail.ru}>
2025-01-22 22:11:43 +0400

    Homework_JAVA_IO_2_22.01.2025

--- src/main/java/ru/otus/java/javaIO2/MainApplicationIO2.java
+++ src/main/java/ru/otus/java/javaIO2/MainApplicationIO2.java
@@ -8,13 +8,14 @@ public class MainApplicationIO2 {
     public static void main(String[] args) throws IOException {
         findingSymbols();
     }
-        private static void findingSymbols () throws IOException {
-            Scanner scanner = new Scanner(System.in);
-            System.out.println("Укажите имя файла, с которым будете работать");
-            String chosenFile = scanner.nextLine();
-            String text = chosenFile.toString();
-            FileReader filereader = new FileReader(text);
-            BufferedReader bufferedReader = new BufferedReader(filereader);
+
+    private static void findingSymbols() throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Укажите имя файла, с которым будете работать");
+        String chosenFile = scanner.nextLine();
+        String text = chosenFile.toString();
+        FileReader filereader = new FileReader(text);
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(chosenFile, StandardCharsets.UTF_8))) {
             String line;
             System.out.print("\nВведите последовательность, которую Вы хотите найти\n");
             String chosenLine = scanner.nextLine();
@@ -23,10 +24,10 @@ public class MainApplicationIO2 {
                 String[] words = line.split(chosenLine);
                 wordCount += words.length - 1;
             }
-            // закрываем файл
             bufferedReader.close();
             System.out.println("Количество" + wordCount);
         }
     }
+}
 
 
