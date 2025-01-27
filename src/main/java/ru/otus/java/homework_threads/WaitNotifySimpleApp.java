98a403f93b74266c0d3e2d82d1bb9aa8328b7779
Irina_Mironova <{missirena@mail.ru}>
2025-01-27 20:46:24 +0400

    homework_threads_fix

--- src/main/java/ru/otus/java/homework_threads/WaitNotifySimpleApp.java
+++ src/main/java/ru/otus/java/homework_threads/WaitNotifySimpleApp.java
@@ -2,101 +2,49 @@ package ru.otus.java.homework_threads;
 
 public class WaitNotifySimpleApp {
 
-    private final Object mon = new Object(); // Монитор для синхронизации
-
+    private final Object mon = new Object();
     private Printer printer = new Printer();
+    private char lastLetter = 'C';
 
     public static void main(String[] args) {
         WaitNotifySimpleApp waitNotifyApp = new WaitNotifySimpleApp();
-        new Thread(() -> waitNotifyApp.printLettterA()).start();
-        new Thread(() -> waitNotifyApp.printLettterB()).start();
-        new Thread(() -> waitNotifyApp.printLettterC()).start();
+        new Thread(() -> waitNotifyApp.printLettter('A')).start();
+        new Thread(() -> waitNotifyApp.printLettter('B')).start();
+        new Thread(() -> waitNotifyApp.printLettter('C')).start();
     }
 
-    public void printLettterA() {
+    public void printLettter(char letter) {
         synchronized (mon) {
             try {
                 for (int i = 0; i < 5; i++) {
-                    while (!printer.isLastLetterC()) {
+                    while (getNextLetter() != letter) {
                         mon.wait();
                     }
-                    printer.printA();
-                    printer.setLastLetter("A");
+                    printer.print(letter);
+                    lastLetter = letter;
                     mon.notifyAll();
                 }
             } catch (InterruptedException e) {
-                e.printStackTrace();
+                Thread.currentThread().interrupt();
+                throw new RuntimeException(e);
             }
         }
     }
-
-    public void printLettterB() {
-        synchronized (mon) {
-            try {
-                for (int i = 0; i < 5; i++) {
-                    while (!printer.isLastLetterA()) {
-                        mon.wait();
-                    }
-                    printer.printB();
-                    printer.setLastLetter("B");
-                    mon.notifyAll();
-                }
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+    private char getNextLetter() {
+        if (lastLetter == 'C') {
+            return 'A';
+        } else if (lastLetter == 'A') {
+            return 'B';
+        } else {
+            return 'C';
         }
     }
 
-    public void printLettterC() {
-        synchronized (mon) {
-            try {
-                for (int i = 0; i < 5; i++) {
-                    while (!printer.isLastLetterB()) {
-                        mon.wait();
-                    }
-                    printer.printC();
-                    printer.setLastLetter("C");
-                    mon.notifyAll();
-                }
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
-    private class Printer {
-        private String lastLetter = "C";
-
-        public void printA() {
-            System.out.print("A");
-        }
-
-        public void printB() {
-            System.out.print("B");
-        }
-
-        public void printC() {
-            System.out.print("C");
-        }
-
-        boolean isLastLetterA() {
-            return "A".equals(lastLetter);
-        }
-
-        boolean isLastLetterB() {
-            return "B".equals(lastLetter);
-        }
-
-        boolean isLastLetterC() {
-            return "C".equals(lastLetter);
-        }
-
-        public void setLastLetter(String lastLetter) {
-            this.lastLetter = lastLetter;
+    private static class Printer {
+        public void print(char letter) {
+            System.out.print(letter);
         }
     }
 }
-
-
 
 
